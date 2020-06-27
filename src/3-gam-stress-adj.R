b8a337ded8ec4e6005a48fc364538303c08198ae
sophttan <46664329+sophttan@users.noreply.github.com>
2020-06-27 15:29:37 -0700

    adjusted analysis (needs time-varying covariates)

--- src/3-gam-stress-adj.R
+++ src/3-gam-stress-adj.R
@@ -80,48 +80,258 @@ Yvars <- c("laz_t2", "waz_t2", "whz_t2" ,"hcz_t2",
            "delta_laz_t2_t3", "delta_waz_t2_t3", "delta_whz_t2_t3", "delta_hcz_t2_t3")
 
 #Fit models
-H1_models <- NULL
+H1_adj_models <- NULL
 for(i in Xvars){
   for(j in Yvars){
     res_adj <- fit_RE_gam(d=d, X=i, Y=j,  W=Wvars)
     res <- data.frame(X=i, Y=j, fit=I(list(res_adj$fit)), dat=I(list(res_adj$dat)))
-    H1_models <- bind_rows(H1_models, res)
+    H1_adj_models <- bind_rows(H1_adj_models, res)
   }
 }
 
 #Get primary contrasts
-H1_res <- NULL
-for(i in 1:nrow(H1_models)){
-  res <- data.frame(X=H1_models$X[i], Y=H1_models$Y[i])
-  preds <- predict_gam_diff(fit=H1_models$fit[i][[1]], d=H1_models$dat[i][[1]], quantile_diff=c(0.25,0.75), Xvar=res$X, Yvar=res$Y)
-  H1_res <-  bind_rows(H1_res , preds$res)
+H1_adj_res <- NULL
+for(i in 1:nrow(H1_adj_models)){
+  res <- data.frame(X=H1_adj_models$X[i], Y=H1_adj_models$Y[i])
+  preds <- predict_gam_diff(fit=H1_adj_models$fit[i][[1]], d=H1_adj_models$dat[i][[1]], quantile_diff=c(0.25,0.75), Xvar=res$X, Yvar=res$Y)
+  H1_adj_res <-  bind_rows(H1_adj_res , preds$res)
 }
-H1_res$adjusted <- 0
+H1_adj_res$adjusted <- 0
 
 #Make list of plots
-H1_plot_list <- NULL
-H1_plot_data <- NULL
-for(i in 1:nrow(H1_models)){
-  res <- data.frame(X=H1_models$X[i], Y=H1_models$Y[i])
-  simul_plot <- gam_simul_CI(H1_models$fit[i][[1]], H1_models$dat[i][[1]], xlab=res$X, ylab=res$Y, title="")
-  H1_plot_list[[i]] <-  simul_plot$p
-  H1_plot_data <-  rbind(H1_plot_data, data.frame(Xvar=res$X, Yvar=res$Y, adj=0, simul_plot$pred %>% subset(., select = c(fit,se.fit,uprP, lwrP,uprS,lwrS))))
+H1_adj_plot_list <- NULL
+H1_adj_plot_data <- NULL
+for(i in 1:nrow(H1_adj_models)){
+  res <- data.frame(X=H1_adj_models$X[i], Y=H1_adj_models$Y[i])
+  simul_plot <- gam_simul_CI(H1_adj_models$fit[i][[1]], H1_adj_models$dat[i][[1]], xlab=res$X, ylab=res$Y, title="")
+  H1_adj_plot_list[[i]] <-  simul_plot$p
+  H1_adj_plot_data <-  rbind(H1_adj_plot_data, data.frame(Xvar=res$X, Yvar=res$Y, adj=0, simul_plot$pred %>% subset(., select = c(fit,se.fit,uprP, lwrP,uprS,lwrS))))
 }
 
 
 #Save models
-saveRDS(H1_models, here("models/H1_models.RDS"))
+saveRDS(H1_adj_models, here("models/H1_adj_models.RDS"))
 
 #Save results
-saveRDS(H1_res, here("results/adjusted/H1_res.RDS"))
+saveRDS(H1_adj_res, here("results/adjusted/H1_adj_res.RDS"))
 
 
 #Save plots
-saveRDS(H1_plot_list, here("figure-objects/H1_adj_splines.RDS"))
+saveRDS(H1_adj_plot_list, here("figure-objects/H1_adj_splines.RDS"))
 
 #Save plot data
-saveRDS(H1_plot_data, here("figure-data/H1_adj_spline_data.RDS"))
+saveRDS(H1_adj_plot_data, here("figure-data/H1_adj_spline_data.RDS"))
 
 
+## Hypothesis 2a
+#Change in slope between pre- and post-stressor cortisol measured at Year 2 is positively associated 
+#with concurrent child LAZ, WAZ, WLZ, and head circumference-for-age Z score at Year 2.
 
+#Exposure: Quartiles of pre- and post-stressor cortisol at Year 2
+#Primary Outcome: Child LAZ at Year 2
+#Secondary Outcome: Child WAZ and head circumference-for-age Z score at Year 2
+#Tertiary Outcome: Child WLZ at Year 2
+
+##Hypothesis 2b
+#Residualized gain score for cortisol measured at Year 2 is positively associated 
+#with concurrent child LAZ, WAZ, WLZ, and head circumference-for-age Z score at Year 2.
+
+#Exposure: Quartiles of pre- and post-stressor cortisol at Year 2
+#Primary Outcome: Child LAZ at Year 2
+#Secondary Outcome: Child WAZ and head circumference-for-age Z score at Year 2
+#Tertiary Outcomes: Child WLZ at Year 2
+
+##Hypothesis 2c
+#Change in slope between pre- and post-stressor alpha-amylase measured at Year 2 is negatively associated 
+#with concurrent child LAZ, WAZ, WLZ, and head circumference-for-age Z score at Year 2.
+
+#Exposure: Quartiles of pre- and post-stressor alpha-amylase at Year 2
+#Primary Outcome: Child LAZ at Year 2
+#Secondary Outcome: Child WAZ and head circumference-for-age Z score at Year 2
+#Tertiary Outcome: Child WLZ at Year 2
+
+##Hypothesis 2d
+#Residualized gain score for alpha-amylase measured at Year 2 is negatively associated 
+#with concurrent child LAZ, WAZ, WLZ, and head circumference-for-age Z score at Year 2.
+
+#Exposure: Quartiles of pre- and post-stressor alpha-amylase at Year 2
+#Primary Outcome: Child LAZ at Year 2
+#Secondary Outcome: Child WAZ and head circumference-for-age Z score at Year 2
+#Tertiary Outcome: Child WLZ at Year 2
+
+Xvars <- c("t3_cort_slope", "t3_residual_cort", "t3_saa_slope", "t3_residual_saa")            
+Yvars <- c("laz_t3", "waz_t3", "whz_t3", "hcz_t3")
+
+#Fit models
+H2_adj_models <- NULL
+for(i in Xvars){
+  for(j in Yvars){
+    res_adj <- fit_RE_gam(d=d, X=i, Y=j,  W=Wvars)
+    res <- data.frame(X=i, Y=j, fit=I(list(res_adj$fit)), dat=I(list(res_adj$dat)))
+    H2_adj_models <- bind_rows(H2_adj_models, res)
+  }
+}
+
+#Get primary contrasts
+H2_adj_res <- NULL
+for(i in 1:nrow(H2_adj_models)){
+  res <- data.frame(X=H2_adj_models$X[i], Y=H2_adj_models$Y[i])
+  preds <- predict_gam_diff(fit=H2_adj_models$fit[i][[1]], d=H2_adj_models$dat[i][[1]], quantile_diff=c(0.25,0.75), Xvar=res$X, Yvar=res$Y)
+  H2_adj_res <-  bind_rows(H2_adj_res , preds$res)
+}
+H2_adj_res$adjusted <- 0
+
+#Make list of plots
+H2_adj_plot_list <- NULL
+H2_adj_plot_data <- NULL
+for(i in 1:nrow(H2_adj_models)){
+  res <- data.frame(X=H2_adj_models$X[i], Y=H2_adj_models$Y[i])
+  simul_plot <- gam_simul_CI(H2_adj_models$fit[i][[1]], H2_adj_models$dat[i][[1]], xlab=res$X, ylab=res$Y, title="")
+  H2_adj_plot_list[[i]] <-  simul_plot$p
+  H2_adj_plot_data <-  rbind(H2_adj_plot_data, data.frame(Xvar=res$X, Yvar=res$Y, adj=0, simul_plot$pred %>% subset(., select = c(fit,se.fit,uprP, lwrP,uprS,lwrS))))
+}
+
+
+#Save models
+saveRDS(H2_adj_models, here("models/H2_adj_models.RDS"))
+
+#Save results
+saveRDS(H2_adj_res, here("results/adjusted/H2_adj_res.RDS"))
+
+
+#Save plots
+saveRDS(H2_adj_plot_list, here("figure-objects/H2_adj_splines.RDS"))
+
+#Save plot data
+saveRDS(H2_adj_plot_data, here("figure-data/H2_adj_spline_data.RDS"))
+
+
+##Hypothesis 3a
+#Mean arterial pressure measured at Year 2 is negatively associated with concurrent 
+#child LAZ, WAZ, WLZ, and head circumference-for-age Z score at Year 2.
+
+#Exposure: Quartiles of mean arterial pressure at Year 2
+#Primary Outcome: Child LAZ at Year 2
+#Secondary Outcome: Child WAZ and head circumference-for-age Z score at Year 2
+#Tertiary Outcomes: Child WLZ at Year 2
+
+##Hypothesis 3b
+#Resting heart rate measured at Year 2 is negatively associated with concurrent 
+#child LAZ, WAZ, WLZ, and head circumference-for-age Z score at Year 2.
+
+#Exposure: Quartiles of resting heart rate at Year 2
+#Primary Outcome: Child LAZ at Year 2
+#Secondary Outcome: Child WAZ and head circumference-for-age Z score at Year 2
+#Tertiary Outcomes: Child WLZ at Year 2
+
+Xvars <- c("t3_map", "t3_hr_mean")            
+Yvars <- c("laz_t3", "waz_t3", "whz_t3", "hcz_t3")
+
+#Fit models
+H3_adj_models <- NULL
+for(i in Xvars){
+  for(j in Yvars){
+    res_adj <- fit_RE_gam(d=d, X=i, Y=j,  W=Wvars)
+    res <- data.frame(X=i, Y=j, fit=I(list(res_adj$fit)), dat=I(list(res_adj$dat)))
+    H3_adj_models <- bind_rows(H3_adj_models, res)
+  }
+}
+
+#Get primary contrasts
+H3_adj_res <- NULL
+for(i in 1:nrow(H3_adj_models)){
+  res <- data.frame(X=H3_adj_models$X[i], Y=H3_adj_models$Y[i])
+  preds <- predict_gam_diff(fit=H3_adj_models$fit[i][[1]], d=H3_adj_models$dat[i][[1]], quantile_diff=c(0.25,0.75), Xvar=res$X, Yvar=res$Y)
+  H3_adj_res <-  bind_rows(H3_adj_res , preds$res)
+}
+H3_adj_res$adjusted <- 0
+
+#Make list of plots
+H3_adj_plot_list <- NULL
+H3_adj_plot_data <- NULL
+for(i in 1:nrow(H3_adj_models)){
+  res <- data.frame(X=H3_adj_models$X[i], Y=H3_adj_models$Y[i])
+  simul_plot <- gam_simul_CI(H3_adj_models$fit[i][[1]], H3_adj_models$dat[i][[1]], xlab=res$X, ylab=res$Y, title="")
+  H3_adj_plot_list[[i]] <-  simul_plot$p
+  H3_adj_plot_data <-  rbind(H3_adj_plot_data, data.frame(Xvar=res$X, Yvar=res$Y, adj=0, simul_plot$pred %>% subset(., select = c(fit,se.fit,uprP, lwrP,uprS,lwrS))))
+}
+
+
+#Save models
+saveRDS(H3_adj_models, here("models/H3_adj_models.RDS"))
+
+#Save results
+saveRDS(H3_adj_res, here("results/adjusted/H3_adj_res.RDS"))
+
+
+#Save plots
+saveRDS(H3_adj_plot_list, here("figure-objects/H3_adj_splines.RDS"))
+
+#Save plot data
+saveRDS(H3_adj_plot_data, here("figure-data/H3_adj_spline_data.RDS"))
+
+
+##Hypothesis 4a
+#Glucocorticoid receptor (NR3C1) exon 1F promoter methylation in saliva samples at Year 2 
+#is negatively associated with concurrent child LAZ, WAZ, WLZ, and head circumference-for-age Z score at Year 2.
+
+#Exposure: Quartiles of overall percentage of methylation across the entire promoter region at Year 2 post-intervention
+#Primary Outcome: Child LAZ at Year 2
+#Secondary Outcome: Child WAZ and head circumference-for-age Z score at Year 2
+#Tertiary Outcomes: Child WLZ at Year 2
+
+##Hypothesis 4b
+#Glucocorticoid receptor NGFI-A transcription factor binding site methylation in saliva samples at Year 2 
+#is negatively associated with concurrent child LAZ, WAZ, WLZ, and head circumference-for-age Z score at Year 2.
+
+#Exposure: Quartiles of percentage methylation at NGFI-A transcription factor binding 	site (CpG site #12)
+#Primary Outcome: Child LAZ at Year 2
+#Secondary Outcome: Child WAZ and head circumference-for-age Z score at Year 2
+#Tertiary Outcomes: Child WLZ at Year 2
+
+Xvars <- c("t3_gcr_mean", "t3_gcr_cpg12")            
+Yvars <- c("laz_t3", "waz_t3", "whz_t3", "hcz_t3")
+
+#Fit models
+H4_adj_models <- NULL
+for(i in Xvars){
+  for(j in Yvars){
+    res_adj <- fit_RE_gam(d=d, X=i, Y=j,  W=Wvars)
+    res <- data.frame(X=i, Y=j, fit=I(list(res_adj$fit)), dat=I(list(res_adj$dat)))
+    H4_adj_models <- bind_rows(H4_adj_models, res)
+  }
+}
+
+#Get primary contrasts
+H4_adj_res <- NULL
+for(i in 1:nrow(H4_adj_models)){
+  res <- data.frame(X=H4_adj_models$X[i], Y=H4_adj_models$Y[i])
+  preds <- predict_gam_diff(fit=H4_adj_models$fit[i][[1]], d=H4_adj_models$dat[i][[1]], quantile_diff=c(0.25,0.75), Xvar=res$X, Yvar=res$Y)
+  H4_adj_res <-  bind_rows(H4_adj_res , preds$res)
+}
+H4_adj_res$adjusted <- 0
+
+#Make list of plots
+H4_adj_plot_list <- NULL
+H4_adj_plot_data <- NULL
+for(i in 1:nrow(H4_adj_models)){
+  res <- data.frame(X=H4_adj_models$X[i], Y=H4_adj_models$Y[i])
+  simul_plot <- gam_simul_CI(H4_adj_models$fit[i][[1]], H4_adj_models$dat[i][[1]], xlab=res$X, ylab=res$Y, title="")
+  H4_adj_plot_list[[i]] <-  simul_plot$p
+  H4_adj_plot_data <-  rbind(H4_adj_plot_data, data.frame(Xvar=res$X, Yvar=res$Y, adj=0, simul_plot$pred %>% subset(., select = c(fit,se.fit,uprP, lwrP,uprS,lwrS))))
+}
+
+
+#Save models
+saveRDS(H4_adj_models, here("models/H4_adj_models.RDS"))
+
+#Save results
+saveRDS(H4_adj_res, here("results/adjusted/H4_adj_res.RDS"))
+
+
+#Save plots
+saveRDS(H4_adj_plot_list, here("figure-objects/H4_adj_splines.RDS"))
+
+#Save plot data
+saveRDS(H4_adj_plot_data, here("figure-data/H4_adj_spline_data.RDS"))
 
