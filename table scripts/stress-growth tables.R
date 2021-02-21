c1569eebc695039d09de09c46dc6d94ed1eb2814
Sophia Tan <sophiatan@berkeley.edu>
2021-02-20 20:16:25 -0800

    updated tables

--- table scripts/stress-growth tables.R
+++ table scripts/stress-growth tables.R
@@ -69,18 +69,9 @@ names(tbl1)<- c("","","","n (%) or median (IQR)")
 #### Table 2 ####
 
 exposure <- c("t2_f2_8ip", "t2_f2_23d", "t2_f2_VI", "t2_f2_12i", "t2_iso_pca")
-outcome <- c("laz_t2", "waz_t2", "whz_t2", "hcz_t2", "laz_t3", "waz_t3", "whz_t3", "hcz_t3",
-             "len_velocity_t2_t3", "wei_velocity_t2_t3", "hc_velocity_t2_t3",
-             "delta_laz_t2_t3", "delta_waz_t2_t3", "delta_whz_t2_t3", "delta_hcz_t2_t3")
+outcome <- c("laz_t2", "waz_t2", "whz_t2", "hcz_t2")
 expo_var <- c("IPF(2a)-III", "2,3-dinor-iPF(2a)-III", "iPF(2a)-VI", "8,12-iso-iPF(2a)-VI", "Combined urinary oxidative stress biomarkers")
-out_var <- c("LAZ Year 1", "WAZ Year 1", "WLZ Year 1", "HCZ Year 1",
-             "LAZ Year 2", "WAZ Year 2", "WLZ Year 2", "HCZ Year 2",
-             "Length velocity (cm/month) Year 1 to Year 2", "Weight velocity (kg/month) Year 1 to Year 2",
-             "Head circumference velocity (cm/month) Year 1 to Year 2",
-             "Change in LAZ from Year 1 to Year 2", 
-             "Change in WAZ from Year 1 to Year 2",
-             "Change in WLZ from Year 1 to Year 2",
-             "Change in HCZ from Year 1 to Year 2")
+out_var <- c("LAZ Year 1", "WAZ Year 1", "WLZ Year 1", "HCZ Year 1")
 
 tbl2 <- growth_tbl("Urinary oxidative stress biomarker", expo_var, out_var, exposure, outcome, H1, H1adj, T)
 tbl2flex <- growth_tbl_flex("Urinary oxidative stress biomarker", expo_var, out_var, exposure, outcome, H1, H1adj, T)
@@ -89,6 +80,47 @@ tbl1flexsupp <- growth_tbl_flex("Urinary oxidative stress biomarker", expo_var, 
 
 #### Table 3 ####
 
+exposure <- c("t2_f2_8ip", "t2_f2_23d", "t2_f2_VI", "t2_f2_12i", "t2_iso_pca")
+outcome <- c("laz_t3", "waz_t3", "whz_t3", "hcz_t3")
+expo_var <- c("IPF(2a)-III", "2,3-dinor-iPF(2a)-III", "iPF(2a)-VI", "8,12-iso-iPF(2a)-VI", "Combined urinary oxidative stress biomarkers")
+out_var <- c("LAZ Year 2", "WAZ Year 2", "WLZ Year 2", "HCZ Year 2")
+
+tbl3 <- growth_tbl("Urinary oxidative stress biomarker", expo_var, out_var, exposure, outcome, H1, H1adj, T)
+tbl3flex <- growth_tbl_flex("Urinary oxidative stress biomarker", expo_var, out_var, exposure, outcome, H1, H1adj, T)
+tbl2supp <- growth_tbl("Urinary oxidative stress biomarker", expo_var, out_var, exposure, outcome, H1, H1adj)
+tbl2flexsupp <- growth_tbl_flex("Urinary oxidative stress biomarker", expo_var, out_var, exposure, outcome, H1, H1adj)
+
+#### Table 4 ####
+
+exposure <- c("t2_f2_8ip", "t2_f2_23d", "t2_f2_VI", "t2_f2_12i", "t2_iso_pca")
+outcome <- c("len_velocity_t2_t3", "wei_velocity_t2_t3", "hc_velocity_t2_t3")
+expo_var <- c("IPF(2a)-III", "2,3-dinor-iPF(2a)-III", "iPF(2a)-VI", "8,12-iso-iPF(2a)-VI", "Combined urinary oxidative stress biomarkers")
+out_var <- c("Length velocity (cm/month) Year 1 to Year 2", 
+             "Weight velocity (kg/month) Year 1 to Year 2",
+             "Head circumference velocity (cm/month) Year 1 to Year 2")
+
+tbl4 <- growth_tbl("Urinary oxidative stress biomarker", expo_var, out_var, exposure, outcome, H1, H1adj, T)
+tbl4flex <- growth_tbl_flex("Urinary oxidative stress biomarker", expo_var, out_var, exposure, outcome, H1, H1adj, T)
+tbl3supp <- growth_tbl("Urinary oxidative stress biomarker", expo_var, out_var, exposure, outcome, H1, H1adj)
+tbl3flexsupp <- growth_tbl_flex("Urinary oxidative stress biomarker", expo_var, out_var, exposure, outcome, H1, H1adj)
+
+#### Table 5 ####
+
+exposure <- c("t2_f2_8ip", "t2_f2_23d", "t2_f2_VI", "t2_f2_12i", "t2_iso_pca")
+outcome <- c("delta_laz_t2_t3", "delta_waz_t2_t3", "delta_whz_t2_t3", "delta_hcz_t2_t3")
+expo_var <- c("IPF(2a)-III", "2,3-dinor-iPF(2a)-III", "iPF(2a)-VI", "8,12-iso-iPF(2a)-VI", "Combined urinary oxidative stress biomarkers")
+out_var <- c("Change in LAZ from Year 1 to Year 2", 
+             "Change in WAZ from Year 1 to Year 2",
+             "Change in WLZ from Year 1 to Year 2",
+             "Change in HCZ from Year 1 to Year 2")
+
+tbl5 <- growth_tbl("Urinary oxidative stress biomarker", expo_var, out_var, exposure, outcome, H1, H1adj, T)
+tbl5flex <- growth_tbl_flex("Urinary oxidative stress biomarker", expo_var, out_var, exposure, outcome, H1, H1adj, T)
+tbl4supp <- growth_tbl("Urinary oxidative stress biomarker", expo_var, out_var, exposure, outcome, H1, H1adj)
+tbl4flexsupp <- growth_tbl_flex("Urinary oxidative stress biomarker", expo_var, out_var, exposure, outcome, H1, H1adj)
+
+#### Table 6 ####
+
 exposure <- c("t3_cort_z01", "t3_cort_z03", "t3_cort_slope", "t3_residual_cort", 
               "t3_saa_z01", "t3_saa_z02", "t3_saa_slope", "t3_residual_saa")
 outcome <- c("laz_t3", "waz_t3", "whz_t3", "hcz_t3")
@@ -96,35 +128,35 @@ expo_var <- c("Pre-stressor cortisol", "Post-stressor cortisol", "Pre to post-st
               "Pre-stressor sAA", "Post-stressor sAA", "Pre to post-stress change in sAA", "sAA residualized gain score")
 out_var <- c("LAZ Year 2", "WAZ Year 2", "WLZ Year 2", "HCZ Year 2")
 
-tbl3 <- growth_tbl("Salivary stress biomarker", expo_var, out_var, exposure, outcome, H2, H2adj, T)
-tbl3flex <- growth_tbl_flex("Salivary stress biomarker", expo_var, out_var, exposure, outcome, H2, H2adj, T)
-tbl2supp <- growth_tbl("Salivary stress biomarker", expo_var, out_var, exposure, outcome, H2, H2adj)
-tbl2flexsupp <- growth_tbl_flex("Salivary stress biomarker", expo_var, out_var, exposure, outcome, H2, H2adj)
+tbl6 <- growth_tbl("Salivary stress biomarker", expo_var, out_var, exposure, outcome, H2, H2adj, T)
+tbl6flex <- growth_tbl_flex("Salivary stress biomarker", expo_var, out_var, exposure, outcome, H2, H2adj, T)
+tbl5supp <- growth_tbl("Salivary stress biomarker", expo_var, out_var, exposure, outcome, H2, H2adj)
+tbl5flexsupp <- growth_tbl_flex("Salivary stress biomarker", expo_var, out_var, exposure, outcome, H2, H2adj)
 
-#### Table 4 ####
+#### Table 7 ####
 
 exposure <- c("t3_map", "t3_hr_mean")
 outcome <- c("laz_t3", "waz_t3", "whz_t3", "hcz_t3")
 expo_var <- c("Mean arterial pressure", "Mean resting heart rate")
 out_var <- c("LAZ Year 2", "WAZ Year 2", "WLZ Year 2", "HCZ Year 2")
 
-tbl4 <- growth_tbl("Resting SAM biomarker", expo_var, out_var, exposure, outcome, H3, H3adj, T)
-tbl4flex <- growth_tbl_flex("Resting SAM biomarker", expo_var, out_var, exposure, outcome, H3, H3adj, T)
-tbl3supp <- growth_tbl("Resting SAM biomarker", expo_var, out_var, exposure, outcome, H3, H3adj)
-tbl3flexsupp <- growth_tbl_flex("Resting SAM biomarker", expo_var, out_var, exposure, outcome, H3, H3adj)
+tbl7 <- growth_tbl("Resting SAM biomarker", expo_var, out_var, exposure, outcome, H3, H3adj, T)
+tbl7flex <- growth_tbl_flex("Resting SAM biomarker", expo_var, out_var, exposure, outcome, H3, H3adj, T)
+tbl6supp <- growth_tbl("Resting SAM biomarker", expo_var, out_var, exposure, outcome, H3, H3adj)
+tbl6flexsupp <- growth_tbl_flex("Resting SAM biomarker", expo_var, out_var, exposure, outcome, H3, H3adj)
 
 
-#### Table 5 ####
+#### Table 8 ####
 
 exposure <- c("t3_gcr_mean", "t3_gcr_cpg12")
 outcome <- c("laz_t3", "waz_t3", "whz_t3", "hcz_t3")
 expo_var <- c("Entire promoter region (39 assayed CpG sites)", "NGFI-A transcription factor binding site (CpG site #12)")
 out_var <- c("LAZ Year 2", "WAZ Year 2", "WLZ Year 2", "HCZ Year 2")
 
-tbl5 <- growth_tbl("Methylation site", expo_var, out_var, exposure, outcome, H4, H4adj, T)
-tbl5flex <- growth_tbl_flex("Methylation site", expo_var, out_var, exposure, outcome, H4, H4adj, T)
-tbl4supp <- growth_tbl("Methylation site", expo_var, out_var, exposure, outcome, H4, H4adj)
-tbl4flexsupp <- growth_tbl_flex("Methylation site", expo_var, out_var, exposure, outcome, H4, H4adj)
+tbl8 <- growth_tbl("Methylation site", expo_var, out_var, exposure, outcome, H4, H4adj, T)
+tbl8flex <- growth_tbl_flex("Methylation site", expo_var, out_var, exposure, outcome, H4, H4adj, T)
+tbl7supp <- growth_tbl("Methylation site", expo_var, out_var, exposure, outcome, H4, H4adj)
+tbl7flexsupp <- growth_tbl_flex("Methylation site", expo_var, out_var, exposure, outcome, H4, H4adj)
 
 
 # #### Supplementary Tables ####
@@ -190,13 +222,24 @@ write.csv(tbl2, here('tables/main/stress-growth-table2.csv'))
 write.csv(tbl3, here('tables/main/stress-growth-table3.csv'))
 write.csv(tbl4, here('tables/main/stress-growth-table4.csv'))
 write.csv(tbl5, here('tables/main/stress-growth-table5.csv'))
+write.csv(tbl6, here('tables/main/stress-growth-table6.csv'))
+write.csv(tbl7, here('tables/main/stress-growth-table7.csv'))
+write.csv(tbl8, here('tables/main/stress-growth-table8.csv'))
 
-save_as_docx("Table 1" = tbl1flex, "Table 2" = tbl2flex, "Table 3" = tbl3flex, "Table 4" = tbl4flex, "Table 5" = tbl5flex, path='C:/Users/Sophia/Documents/WASH/WASH Stress and Growth/stress-growth main v5.docx')
+save_as_docx("Table 1" = tbl1flex, "Table 2" = tbl2flex, "Table 3" = tbl3flex, "Table 4" = tbl4flex, 
+             "Table 5" = tbl5flex, "Table 6" = tbl5flex, "Table 7" = tbl5flex, "Table 8" = tbl5flex, 
+             path='C:/Users/Sophia/Documents/WASH/WASH Stress and Growth/stress-growth main v6.docx')
 
 write.csv(tbl1supp, here('tables/supplementary/stress-growth-tables1.csv'))
 write.csv(tbl2supp, here('tables/supplementary/stress-growth-tables2.csv'))
 write.csv(tbl3supp, here('tables/supplementary/stress-growth-tables3.csv'))
 write.csv(tbl4supp, here('tables/supplementary/stress-growth-tables4.csv'))
+write.csv(tbl5supp, here('tables/supplementary/stress-growth-tables5.csv'))
+write.csv(tbl6supp, here('tables/supplementary/stress-growth-tables6.csv'))
+write.csv(tbl7supp, here('tables/supplementary/stress-growth-tables7.csv'))
 
-save_as_docx("Table S1" = tbl1flexsupp, "Table S2" = tbl2flexsupp, "Table S3" = tbl3flexsupp, "Table S4" = tbl4flexsupp, path='C:/Users/Sophia/Documents/WASH/WASH Stress and Growth/stress-growth supplementary v5.docx')
+
+save_as_docx("Table S1" = tbl1flexsupp, "Table S2" = tbl2flexsupp, "Table S3" = tbl3flexsupp, 
+             "Table S4" = tbl4flexsupp, "Table S5" = tbl5flexsupp, "Table S6" = tbl6flexsupp, "Table S7" = tbl7flexsupp, 
+             path='C:/Users/Sophia/Documents/WASH/WASH Stress and Growth/stress-growth supplementary v6.docx')
 
